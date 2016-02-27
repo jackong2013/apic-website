a141db0137fd6d6aabc6a1b2ad6e73340ce8185c
sswander <leonardosyahputra@yahoo.com>
2016-02-26 22:45:14 -0800

    first commit for landing page

--- apic.py
+++ apic.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def landing_page():
+    return render_template('index.html')
+
+url_for('static', filename='style.css')
 
 if __name__ == '__main__':
     app.run()
