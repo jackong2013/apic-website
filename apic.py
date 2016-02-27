4436eb076b9b0ddb426d0f9f683e8c82f5e3c3d1
sswander <leonardosyahputra@yahoo.com>
2016-02-27 12:01:01 -0800

    included static url for apic.py, changes in css

--- apic.py
+++ apic.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def landing_page():
-    return render_template('index.html')
+	url_for('static', filename='style.css')
+	return render_template('index.html')
 
-url_for('static', filename='style.css')
 
 if __name__ == '__main__':
     app.run()
