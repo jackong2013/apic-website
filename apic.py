f8b86c819247517d842e142f8e6726626ca7f5a8
Jack Ong <jackong2013@gmail.com>
2016-02-28 03:12:15 -0800

    merchant console home page styling

--- apic.py
+++ apic.py
@@ -8,9 +8,23 @@ def landing_page():
 	return render_template('index.html')
 
 
-@app.route('/<merchant>')
-def console_page(merchant):
-    return render_template('console.html', merchant=merchant)
+MERCHANT_DATA = {
+    "name": "Chocolate Origin",
+    "incoming": [{ "name": "Jack Ong", 
+                 "eta": "5mins" },
+               { "name": "Scott Chue", 
+                 "eta": "7mins" },
+               { "name": "Kang Soon", 
+                 "eta": "10mins" },
+               { "name": "Leo Sjahputra", 
+                 "eta": "14mins" },
+               { "name": "Leo Sjahputra", 
+                 "eta": "14mins" }]
+}
+
+@app.route('/chocolateorigin')
+def console_page():
+    return render_template('console.html', data=MERCHANT_DATA)
 
 if __name__ == '__main__':
     app.run()
