6f391b1fcc0fa2109a36bf245fcf856bd50b3e77
Jack Ong <jackong2013@gmail.com>
2016-02-28 09:35:40 -0800

    added main functions - pseudo code

--- apic.py
+++ apic.py
@@ -1,5 +1,6 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 import requests
+import twitter as twit
 
 app = Flask(__name__)
 
@@ -35,6 +36,52 @@ MERCHANT_DATA = {
 @app.route('/chocolateorigin')
 def console_page():
     return render_template('console.html', data=MERCHANT_DATA)
+
+
+@app.route('/challenge/<merchant_id>', method='POST')
+def new_challenge(merchant_id):
+    data = { "merchant_id": merchant_id,
+    "_title": request.args.get('photo'),
+    "_desc": request.args.get('desc'),
+    "_photo": request.files['photo'],
+    "_start_date": request.args.get('start'),
+    "_end_date": request.args.get('end'),
+    "_hashtag": request.args.get('hashtag')}
+    insert_into_table("challenges", data)
+    twit.post_challenge(merchant_id, data)
+
+@app.route('/coupon/<merchant_id>', method='POST')
+def new_coupon(merchant_id):
+    data = { 
+        "merchant_id": merchant_id,
+        "_title": request.args.get('photo'),
+        "_desc": request.args.get('desc'),
+        "_photo": request.files['photo'],
+        "_start_date": request.args.get('start'),
+        "_end_date": request.args.get('end'),
+        "_hashtag": request.args.get('hashtag'),
+        "_price_usd": request.args.get('price'),
+        "_max_number": request.args.get('max_number')
+    }
+    insert_into_table("coupons", data)
+    twit.post_coupon(merchant_id, data)
+
+@app.route('/uber/<user_id>/<merchant_id>/<request_id>', method='POST')
+def request_uber(user_id, merchant_id, request_id):
+    data = {
+        "user_id": user_id,
+        "merchant_id": merchant_id,
+        "request_id": request_id,
+        "_challenge_id": request.args.get("challenge_id"),
+        "_start_lat": request.args.get("start_lat"),
+        "_start_lon": request.args.get("start_lon"),
+        "_end_lat": request.args.get("end_lat"),
+        "_end_lon": request.args.get("end_lon")
+    }
+    insert_into_table("uber_requests", data)
+    notify_merchant(merchant_id, data)
+    feed = tunnel_uber(request_id)
+    feedback_user(user_id, feed)
 
 if __name__ == '__main__':
     app.run()
