bdc051369691e4eedb635264db0aa082d237972c
Jack Ong <jackong2013@gmail.com>
2016-02-28 07:16:58 -0800

    Updated incoming users

--- apic.py
+++ apic.py
@@ -11,15 +11,25 @@ def landing_page():
 MERCHANT_DATA = {
     "name": "Chocolate Origin",
     "incoming": [{ "name": "Jack Ong", 
-                 "eta": "5mins" },
+                 "eta": 5,
+                 "friend": 2,
+                 "img": "jack.png" },
                { "name": "Scott Chue", 
-                 "eta": "7mins" },
+                 "eta": 7,
+                 "friend": 3,
+                 "img": "scott.png" },
                { "name": "Kang Soon", 
-                 "eta": "10mins" },
+                 "eta": 10,
+                 "friend": 3,
+                 "img": "kang.png" },
                { "name": "Leo Sjahputra", 
-                 "eta": "14mins" },
+                 "eta": 14,
+                 "friend": 4,
+                 "img": "leo.png" },
                { "name": "Chua Chin Siang", 
-                 "eta": "14mins" }]
+                 "eta": 14,
+                 "friend": 1,
+                 "img": "ccs.png" }]
 }
 
 @app.route('/chocolateorigin')
